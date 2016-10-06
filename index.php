3c84090460032797fd77eaa83f55e23202a4fdcb
icruzg <ivan.1133@gmail.com>
2016-10-06 14:52:56 -0500

    primera
    inicio proyecto

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html ng-app lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Punto de Venta</title>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+
+	<link rel="stylesheet" href="bower_components/lumx/dist/lumx.css">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    
+	<script type="text/javascript" src="js/angular.min.js"></script>
+</head>
+<body>
+	<p class="Titulo"><h1>Punto de Venta </h1><strong> Tortiplus</strong> </p>
+	<label for="nombre">Usuario</label>
+	<input type="text" name="nombre" id="nombre">
+	<label for="password">Password</label>
+	<input type="password" name="password" id="password">
+	<a href="#">entrar</a>
+
+
+</body>
+</html>
