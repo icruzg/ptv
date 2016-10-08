3d3e9efedd14b924d19b85d96125dabb7adbc162
Ivan Cruz Garcia <ivan.1133@gmail.com>
2016-10-07 19:24:05 -0500

    un poco de formato

--- index.php
+++ index.php
@@ -11,13 +11,22 @@
 	<script type="text/javascript" src="js/angular.min.js"></script>
 </head>
 <body>
-	<p class="Titulo"><h1>Punto de Venta </h1><strong> Tortiplus</strong> </p>
-	<label for="nombre">Usuario</label>
-	<input type="text" name="nombre" id="nombre">
-	<label for="password">Password</label>
-	<input type="password" name="password" id="password">
-	<a href="#">entrar</a>
-
-	
+	<section class="login">
+		<p class="Titulo"><h1>Punto de Venta <span> Tortiplus</span></h1> </p>
+		<label for="nombre">Usuario</label>
+		<input type="text" name="nombre" id="nombre">
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password">
+		<a href="#">entrar</a>
+	</section>
+	<footer>
+		<div class="link">
+			<ul>
+				<li><a href="#">Contactanos</a> |</li>
+				<li><a href="#">Cambiar contraseña </a>|</li>
+				<li><a href="#">olvide mi contraseña</a>|</li>
+			</ul>
+		</div>
+	</footer>
 </body>
 </html>
